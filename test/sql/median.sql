6af5d0c6ea278358eae0f9a9993ad6409edf5174
Lakshmi Narayanan Sreethar <slnarayanan.tvl@gmail.com>
2022-08-01 19:55:41 +0530

    Added testcases
    Added a few more tests to verify the behaviour of collation handling in
    text comparators and parallel, partial aggregate mode.

--- test/sql/median.sql
+++ test/sql/median.sql
@@ -59,8 +59,72 @@ SELECT median(val) FROM textvals;
 -- Test large table with timestamps
 CREATE TABLE timestampvals (val timestamptz);
 
+SET max_parallel_workers_per_gather=4;
+
 INSERT INTO timestampvals(val)
 SELECT TIMESTAMP 'epoch' + (i * INTERVAL '1 second')
-FROM generate_series(0, 100000) as T(i);
+FROM generate_series(0, 10000000) as T(i);
 
+-- expected median
+SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY val) as expected_median
+FROM timestampvals;
+-- actual median
 SELECT median(val) FROM timestampvals;
+
+-- Test collation handling by text comparators;
+CREATE COLLATION numeric (provider = icu, locale = 'en@colNumeric=yes');
+CREATE TABLE textvals2(valDefaultCollation text, valNumericOrder text COLLATE numeric);
+INSERT INTO textvals2 VALUES
+         ('a1', 'a1'),
+         ('a2', 'a2'),
+         ('a3', 'a3'),
+         ('a4', 'a4'),
+         ('a5', 'a5'),
+         ('a6', 'a6'),
+         ('a10', 'a10'),
+         ('a20', 'a20'),
+         ('a30', 'a30'),
+         ('a40', 'a40'),
+         ('a50', 'a50');
+
+-- expected median with default collation
+SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY valDefaultCollation) as expected_median
+FROM textvals2;
+-- actual median with default collation
+SELECT median(valDefaultCollation) FROM textvals2;
+
+-- expected median with numeric collation
+SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY valNumericOrder) as expected_median
+FROM textvals2;
+-- actual median with numeric collation
+SELECT median(valNumericOrder) FROM textvals2;
+
+-- Test large table with text
+CREATE TABLE textvalslarge (val text);
+
+INSERT INTO textvalslarge(val)
+SELECT 'TEXT-' || i
+FROM generate_series(0, 1000000) as T(i);
+
+-- expected median
+SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY val) as expected_median
+FROM textvalslarge;
+-- actual median
+SELECT median(val) FROM textvalslarge;
+
+-- Test large table with smallint
+CREATE TABLE smallintvals(val smallint);
+
+INSERT INTO smallintvals(val)
+SELECT
+    CASE
+        WHEN i % 2 = 0 THEN i % 32767
+        ELSE -i % 32767
+    END
+FROM generate_series(0, 10000000) as T(i);
+
+-- expected median
+SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY val) as expected_median
+FROM smallintvals;
+-- actual median
+SELECT median(val) FROM smallintvals;
